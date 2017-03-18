bb64031c8660c643e16f6677aa3b59b099bc5e40
Andrei Samkov <andrei.samkov@gmail.com>
2017-03-18 22:46:05 +0200

    Output stream callback

--- src/main/java/ee/smkv/executor/remote/web/SshRemoteExecutor.java
+++ src/main/java/ee/smkv/executor/remote/web/SshRemoteExecutor.java
@@ -1,27 +1,33 @@
 package ee.smkv.executor.remote.web;
 
 import com.jcraft.jsch.JSchException;
-import ee.smkv.executor.remote.*;
+import ee.smkv.executor.remote.Config;
+import ee.smkv.executor.remote.Executor;
+import ee.smkv.executor.remote.OutputStreamCallback;
+import ee.smkv.executor.remote.SshServer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.ParseException;
 
 @Controller
 @RequestMapping("/executor")
 public class SshRemoteExecutor {
+    @RequestMapping()
+    public String index(){
+        return "executor";
+    }
 
     @RequestMapping(value = "/execute", method = RequestMethod.POST)
-    @ResponseBody
-    public String execute(String command) throws IOException, JSchException, ParseException {
+    public void execute(String command, HttpServletResponse response) throws IOException, JSchException, ParseException {
         SshServer sshServer = SshServer.fromString(Config.getProperty("remote.server"));
         String privateKey = Config.getProperty("keys.private");
         Executor executor = new Executor(sshServer, privateKey);
-        StringBufferCallback stringBufferCallback = new StringBufferCallback();
-        executor.execute(command, stringBufferCallback);
-        return stringBufferCallback.toString();
+        OutputStreamCallback callback = new OutputStreamCallback(response.getOutputStream());
+        executor.execute(command, callback);
+
     }
 }
